bb669277bfa51f1ffd0b444508a592789b6e4b92
Greg Bowler <greg.bowler@g105b.com>
2014-10-02 20:37:05 +0100

    Transition on transforms for smooth animation

--- script.js
+++ script.js
@@ -5,8 +5,26 @@ var
 $;
 
 function setLefts() {
+	var
+		i = 0,
+	$;
+
 	[].forEach.call(this.querySelectorAll(".card"), function(card) {
-		card.style.left = [card.parentNode.offsetLeft, "px"].join("");
+		var
+			left = [card.parentNode.offsetLeft, "px"].join(""),
+			translatePercent = 150 * i,
+		$;
+
+		card.style.transform = [
+			"translateY(55vw)",
+			"scale(0.125)",
+			"translateX(-150%)",
+			"translateX(",
+			translatePercent,
+			"%)",
+		].join("");
+
+		++i;
 	});
 }
 
